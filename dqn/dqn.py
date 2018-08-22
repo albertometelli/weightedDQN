4dffced2fbda3077b7e2a16bb7684464776fc6de
albertometelli <alberto.metelli@gmail.com>
2018-08-22 15:07:58 +0200

    fixed compatibility

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -5,16 +5,16 @@ import numpy as np
 from mushroom.algorithms.agent import Agent
 from mushroom.approximators.regressor import Ensemble, Regressor
 
-from replay_memory import Buffer, ReplayMemory
+from replay_memory import ReplayMemory
 
 
 class DQN(Agent):
     def __init__(self, approximator, policy, mdp_info, batch_size,
                  target_update_frequency, initial_replay_size, train_frequency,
                  max_replay_size, fit_params=None, approximator_params=None,
-                 n_approximators=1, history_length=1, clip_reward=True,
-                 max_no_op_actions=0, no_op_action_value=0, p_mask=2 / 3.,
-                 dtype=np.float32, weighted_update=False):
+                 n_approximators=1, clip_reward=True,
+                 max_no_op_actions=0, no_op_action_value=0,
+                 weighted_update=False):
         self._fit_params = dict() if fit_params is None else fit_params
 
         self._batch_size = batch_size
@@ -23,17 +23,11 @@ class DQN(Agent):
         self._target_update_frequency = target_update_frequency // train_frequency
         self._max_no_op_actions = max_no_op_actions
         self._no_op_action_value = no_op_action_value
-        self._p_mask = p_mask
-        self.weighted_update=weighted_update
-        self._replay_memory = ReplayMemory(
-            mdp_info, initial_replay_size, max_replay_size, history_length,
-            n_approximators, dtype
-        )
-        self._buffer = Buffer(history_length, dtype)
+        self.weighted_update = weighted_update
+
+        self._replay_memory = ReplayMemory(initial_replay_size, max_replay_size)
 
         self._n_updates = 0
-        self._episode_steps = 0
-        self._no_op_actions = None
 
         apprx_params_train = deepcopy(approximator_params)
         apprx_params_train['name'] = 'train'
@@ -51,8 +45,7 @@ class DQN(Agent):
 
     def fit(self, dataset):
         
-        mask = np.ones((len(dataset),
-                                        self._n_approximators))
+        mask = np.ones((len(dataset), self._n_approximators))
         self._replay_memory.add(dataset, mask)
         if self._replay_memory.initialized:
             state, action, reward, next_state, absorbing, _, mask =\
@@ -117,27 +110,11 @@ class DQN(Agent):
 
 
     def draw_action(self, state):
-        self._buffer.add(state)
-
-        if self._episode_steps < self._no_op_actions:
-            action = np.array([self._no_op_action_value])
-            self.policy.update_epsilon(state)
-        else:
-            extended_state = self._buffer.get()
-
-            action = super(DQN, self).draw_action(extended_state)
-
-        self._episode_steps += 1
+        action = super(DQN, self).draw_action(np.array(state))
 
         return action
 
     def episode_start(self):
-        if self._max_no_op_actions == 0:
-            self._no_op_actions = 0
-        else:
-            self._no_op_actions = np.random.randint(
-                self._buffer.size, self._max_no_op_actions + 1)
-        self._episode_steps = 0
         self.policy.set_idx(np.random.randint(self._n_approximators))
 
 
